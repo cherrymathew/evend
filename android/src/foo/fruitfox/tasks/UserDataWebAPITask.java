37f6503c75b8c15ebe6db1ec19e09460a032f141
fraggerfox <santhosh.raju@gmail.com>
2015-03-06 20:53:25 +0530

    Implemented an interface to facilitate easier callbacks in classes that use Async HTTP calls
    Removed the dependency of "MainActivity" class now, it takes the Activity generic object

--- android/src/foo/fruitfox/tasks/UserDataWebAPITask.java
+++ android/src/foo/fruitfox/tasks/UserDataWebAPITask.java
@@ -1,28 +1,32 @@
 package foo.fruitfox.tasks;
 
+import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.os.AsyncTask;
-import foo.fruitfox.evend.MainActivity;
 import foo.fruitfox.helpers.DebugHelper;
 import foo.fruitfox.helpers.NetworkHelper;
 
 public class UserDataWebAPITask extends AsyncTask<String, Integer, String> {
 	private ProgressDialog progDialog;
 	private Context context;
-	private MainActivity activity;
+	private Activity activity;
+	private AsyncResponse ar;
 
-	// TODO: Need to do something about Activity class type
+	public interface AsyncResponse {
+		void postAsyncTaskCallback(String result);
+	}
 
 	/**
 	 * Construct a task
 	 * 
 	 * @param activity
 	 */
-	public UserDataWebAPITask(MainActivity activity) {
+	public UserDataWebAPITask(Activity activity) {
 		super();
 		this.activity = activity;
 		this.context = this.activity.getApplicationContext();
+		this.ar = (AsyncResponse) activity;
 	}
 
 	@Override
@@ -53,7 +57,7 @@ public class UserDataWebAPITask extends AsyncTask<String, Integer, String> {
 					"Unable to find track data. Try again later.");
 			return;
 		} else {
-			DebugHelper.ShowMessage.t(context, result);
+			ar.postAsyncTaskCallback(result);
 			return;
 		}
 
