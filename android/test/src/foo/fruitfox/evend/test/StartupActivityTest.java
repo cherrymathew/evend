d1ab94cd3f0be6893cd616670375784cd633865f
fraggerfox <santhosh.raju@gmail.com>
2015-03-12 21:50:28 +0530

    Added a basic set of unit tests for Statup Activity

--- android/test/src/foo/fruitfox/evend/test/StartupActivityTest.java
+++ android/test/src/foo/fruitfox/evend/test/StartupActivityTest.java
@@ -1,12 +1,17 @@
 package foo.fruitfox.evend.test;
 
-import android.test.ActivityInstrumentationTestCase2;
+import android.content.Intent;
+import android.view.ContextThemeWrapper;
+import android.widget.Button;
+import foo.fruitfox.evend.R;
 import foo.fruitfox.evend.StartupActivity;
 
 public class StartupActivityTest extends
-		ActivityInstrumentationTestCase2<StartupActivity> {
+		android.test.ActivityUnitTestCase<StartupActivity> {
 
 	private StartupActivity startupActivity;
+	private Button phoneVerification;
+	private Button emailVerification;
 
 	public StartupActivityTest() {
 		super(StartupActivity.class);
@@ -14,14 +19,57 @@ public class StartupActivityTest extends
 
 	protected void setUp() throws Exception {
 		super.setUp();
+		ContextThemeWrapper context = new ContextThemeWrapper(
+				getInstrumentation().getTargetContext(), R.style.AppTheme);
+		setActivityContext(context);
+
+		Intent intent = new Intent(getInstrumentation().getTargetContext(),
+				StartupActivity.class);
+		startActivity(intent, null, null);
+
 		startupActivity = getActivity();
+
+		phoneVerification = (Button) startupActivity
+				.findViewById(R.id.phoneVerification);
+		emailVerification = (Button) startupActivity
+				.findViewById(R.id.emailVerification);
 	}
 
 	public void testPreconditions() {
-		// Try to add a message to add context to your assertions. These
-		// messages will be shown if a tests fails and make it easy to
-		// understand why a test failed
 		assertNotNull("startupActivity is null", startupActivity);
+		assertNotNull("phoneVerification is null", phoneVerification);
+		assertNotNull("emailVerification is null", emailVerification);
 	}
 
+	public void testPhoneVerificationButtonText() {
+		assertEquals("Incorrect text for Phone Registration Button",
+				"Phone Number Registration", phoneVerification.getText());
+	}
+
+	public void testEmailVerificationButtonText() {
+		assertEquals("Incorrect text for Email Registration Button",
+				"E-mail Registration", emailVerification.getText());
+	}
+
+	public void testPhoneVerificationButtonClick() {
+		phoneVerification.performClick();
+
+		Intent triggeredIntent = getStartedActivityIntent();
+		assertNotNull("Intent was null", triggeredIntent);
+
+		String data = triggeredIntent.getExtras().getString("type");
+
+		assertEquals("Incorrect value recieved", "phone", data);
+	}
+
+	public void testEmailVerificationButtonClick() {
+		emailVerification.performClick();
+
+		Intent triggeredIntent = getStartedActivityIntent();
+		assertNotNull("Intent was null", triggeredIntent);
+
+		String data = triggeredIntent.getExtras().getString("type");
+
+		assertEquals("Incorrect value recieved", "email", data);
+	}
 }
