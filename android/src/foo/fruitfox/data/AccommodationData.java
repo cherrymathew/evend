cd3f342e07c36483cb73a9984d106a4102ba03c6
fraggerfox <santhosh.raju@gmail.com>
2015-04-04 18:43:52 +0530

    Added the ability to select a Date to book the accommodation and also add the number of days for which the booking is done.

--- android/src/foo/fruitfox/data/AccommodationData.java
+++ android/src/foo/fruitfox/data/AccommodationData.java
@@ -1,13 +1,24 @@
 package foo.fruitfox.data;
 
+import java.util.TimeZone;
+
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
 public class AccommodationData {
 	private String accommodationType;
 	private String bedsCount;
+	private DateTime accommodationStartDate;
+	private String daysCount;
 
 	public AccommodationData() {
 		super();
 		this.accommodationType = "";
 		this.bedsCount = "";
+		this.accommodationStartDate = null;
+		this.daysCount = "";
 	}
 
 	public AccommodationData(String accommodationType, String bedsCount) {
@@ -32,4 +43,60 @@ public class AccommodationData {
 		this.bedsCount = bedsCount;
 	}
 
+	public DateTime getStartDate() {
+		return accommodationStartDate;
+	}
+
+	public void setStartDate(DateTime startDate) {
+		this.accommodationStartDate = startDate;
+	}
+
+	public String getDaysCount() {
+		return daysCount;
+	}
+
+	public void setDaysCount(String daysCount) {
+		this.daysCount = daysCount;
+	}
+
+	/**
+	 * @param pattern
+	 *            for the date string to be returned. All the patterns adhere to
+	 *            the Joda Time string standards.
+	 * 
+	 * @see Link
+	 *      http://joda-time.sourceforge.net/apidocs/org/joda/time/format/
+	 *      DateTimeFormat.html
+	 * @return The formatted date string according to the pattern passed.
+	 */
+	public String getStartDate(String pattern) {
+		String dateString = "";
+
+		if (this.accommodationStartDate != null) {
+			DateTimeFormatter dtf = DateTimeFormat.forPattern(pattern);
+			dateString = dtf.print(this.accommodationStartDate);
+		}
+
+		return dateString;
+	}
+
+	/**
+	 * 
+	 * @param date
+	 *            string matching the passed date pattern.
+	 * @param pattern
+	 *            for the date string to be returned. All the patterns adhere to
+	 *            the Joda Time string standards.
+	 * @see Link
+	 *      http://joda-time.sourceforge.net/apidocs/org/joda/time/format/
+	 *      DateTimeFormat.html
+	 */
+	public void setStartDate(String pattern, String date) {
+		if (date.length() > 0 && pattern.length() > 0) {
+			String currentTimeZone = TimeZone.getDefault().getID();
+			DateTimeFormatter dtf = DateTimeFormat.forPattern(pattern);
+			this.accommodationStartDate = dtf.parseDateTime(date);
+			this.accommodationStartDate.withZone(DateTimeZone.forID(currentTimeZone));
+		}
+	}
 }
--- android/src/foo/fruitfox/evend/AccomodationActivity.java
+++ android/src/foo/fruitfox/evend/AccomodationActivity.java
@@ -1,32 +1,50 @@
 package foo.fruitfox.evend;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
+import android.app.Dialog;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnFocusChangeListener;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.CalendarView.OnDateChangeListener;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
 import foo.fruitfox.data.AccommodationData;
 import foo.fruitfox.data.UserData;
 import foo.fruitfox.helpers.StorageHelper;
 
 public class AccomodationActivity extends ActionBarActivity implements
-		OnItemSelectedListener {
+		OnItemSelectedListener, OnClickListener, OnFocusChangeListener {
 	private List<String> accommodationTypesList;
 	private List<String> bedsCountList;
+	private List<String> daysCountList;
 
 	private ArrayAdapter<String> accommodationTypesAdapter;
 	private ArrayAdapter<String> bedsCountAdapter;
+	private ArrayAdapter<String> daysCountAdapter;
 
 	private Spinner accommodationTypes;
 	private Spinner bedsCount;
+	private Spinner daysCount;
 
 	private String identifier;
 	private UserData userData;
@@ -34,10 +52,14 @@ public class AccomodationActivity extends ActionBarActivity implements
 
 	private boolean hasPickup;
 
+	private Context context;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_accomodation);
+
+		context = this;
 
 		identifier = StorageHelper.PreferencesHelper.getIdentifier(this);
 		userData = StorageHelper.PreferencesHelper
@@ -45,6 +67,11 @@ public class AccomodationActivity extends ActionBarActivity implements
 
 		accommodationTypes = (Spinner) findViewById(R.id.accommodationTypes);
 		bedsCount = (Spinner) findViewById(R.id.bedsCount);
+		daysCount = (Spinner) findViewById(R.id.daysCount);
+
+		accommodationTypes.setFocusable(true);
+		accommodationTypes.setFocusableInTouchMode(true);
+		accommodationTypes.requestFocus();
 
 		Intent intent = getIntent();
 		hasPickup = intent.getBooleanExtra("hasPickup", false);
@@ -76,6 +103,9 @@ public class AccomodationActivity extends ActionBarActivity implements
 		super.onResume();
 		int position = 0;
 
+		EditText accommodationStartDate = (EditText) findViewById(R.id.accommodationStartDate);
+		String dateString = "";
+
 		userData = StorageHelper.PreferencesHelper
 				.getUserData(this, identifier);
 
@@ -90,6 +120,15 @@ public class AccomodationActivity extends ActionBarActivity implements
 					.getBedsCount());
 			bedsCount.setSelection(position);
 
+			position = daysCountAdapter.getPosition(accommodationData
+					.getDaysCount());
+			daysCount.setSelection(position);
+
+			dateString = accommodationData.getStartDate("dd-MM-yyyy");
+			if (dateString.length() > 0) {
+				accommodationStartDate.setText(dateString);
+			}
+
 		} else {
 			accommodationData = new AccommodationData();
 		}
@@ -98,6 +137,11 @@ public class AccomodationActivity extends ActionBarActivity implements
 	protected void onPause() {
 		super.onPause();
 
+		EditText accommodationStartDate = (EditText) findViewById(R.id.accommodationStartDate);
+
+		accommodationData.setStartDate("dd-MM-yyyy", accommodationStartDate
+				.getText().toString());
+
 		userData.setAccommodationData(accommodationData);
 
 		StorageHelper.PreferencesHelper.setUserData(this, identifier, userData);
@@ -105,14 +149,19 @@ public class AccomodationActivity extends ActionBarActivity implements
 
 	private void initializeAdapters() {
 		accommodationTypesList = new ArrayList<String>();
+		bedsCountList = new ArrayList<String>();
+		daysCountList = new ArrayList<String>();
+
 		accommodationTypesList.add("Room");
 		accommodationTypesList.add("Bed");
 		accommodationTypesList.add("Tent");
 
-		bedsCountList = new ArrayList<String>();
-
 		for (int i = 1; i < 6; i++) {
 			bedsCountList.add("" + i);
+		}
+
+		for (int i = 1; i < 15; i++) {
+			daysCountList.add("" + i);
 		}
 
 		accommodationTypesAdapter = new ArrayAdapter<String>(this,
@@ -127,22 +176,115 @@ public class AccomodationActivity extends ActionBarActivity implements
 		bedsCountAdapter
 				.setDropDownViewResource(android.R.layout.simple_expandable_list_item_1);
 
+		daysCountAdapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_expandable_list_item_1, daysCountList);
+
+		daysCountAdapter
+				.setDropDownViewResource(android.R.layout.simple_expandable_list_item_1);
+
 		accommodationTypes.setAdapter(accommodationTypesAdapter);
 		bedsCount.setAdapter(bedsCountAdapter);
+		daysCount.setAdapter(daysCountAdapter);
 	}
 
 	private void initializeListeners() {
+		EditText accommodationStartDate = (EditText) findViewById(R.id.accommodationStartDate);
+
 		accommodationTypes.setOnItemSelectedListener(this);
 		bedsCount.setOnItemSelectedListener(this);
+		daysCount.setOnItemSelectedListener(this);
+		accommodationStartDate.setOnClickListener(this);
+		accommodationStartDate.setOnFocusChangeListener(this);
+	}
+
+	private void displayCalendar() {
+		final Dialog dialog = new Dialog(context);
+		EditText accommodationStartDate = (EditText) findViewById(R.id.accommodationStartDate);
+
+		DateTime startDate = new DateTime("2015-05-23");
+		DateTime endDate = new DateTime("2015-06-07");
+
+		dialog.setTitle("Select your date");
+		dialog.setContentView(R.layout.calendar_date_picker);
+
+		Button acceptDate = (Button) dialog.findViewById(R.id.acceptDate);
+
+		CalendarView calendar = (CalendarView) dialog
+				.findViewById(R.id.eventCalendarView);
+
+		// sets whether to show the week number.
+		calendar.setShowWeekNumber(false);
+
+		calendar.setMinDate(startDate.getMillis());
+		calendar.setMaxDate(endDate.getMillis());
+
+		calendar.setMinimumHeight(50);
+
+		// sets the first day of week according to Calendar.
+		// here we set Monday as the first day of the Calendar
+		calendar.setFirstDayOfWeek(Calendar.MONDAY);
+
+		// The background color for the selected week.
+		calendar.setSelectedWeekBackgroundColor(getResources().getColor(
+				R.color.aqua));
+
+		// sets the color for the vertical bar shown at the beginning and at
+		// the end of the selected date.
+		calendar.setSelectedDateVerticalBar(R.color.silver);
+
+		if (accommodationStartDate.getText().toString().length() > 0) {
+			DateTimeFormatter dtf = DateTimeFormat.forPattern("dd-MM-yyyy");
+			DateTime currentSelectedDate = dtf
+					.parseDateTime(accommodationStartDate.getText().toString());
+			calendar.setDate(currentSelectedDate.getMillis());
+		}
+
+		// sets the listener to be notified upon selected date change.
+		calendar.setOnDateChangeListener(new OnDateChangeListener() {
+
+			@Override
+			public void onSelectedDayChange(CalendarView view, int year,
+					int month, int dayOfMonth) {
+				LinearLayout accommodationStartDateLayout = (LinearLayout) findViewById(R.id.accommodationStartDateContainer);
+				EditText accommodationStartDate = (EditText) accommodationStartDateLayout
+						.findViewById(R.id.accommodationStartDate);
+				accommodationStartDate.setText(dayOfMonth + "-" + (month + 1)
+						+ "-" + year);
+				dialog.dismiss();
+			}
+		});
+
+		acceptDate.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View view) {
+				LinearLayout accommodationStartDateLayout = (LinearLayout) findViewById(R.id.accommodationStartDateContainer);
+				EditText accommodationStartDate = (EditText) accommodationStartDateLayout
+						.findViewById(R.id.accommodationStartDate);
+
+				if (accommodationStartDate.getText().toString().length() == 0) {
+					accommodationStartDate.setText("23-05-2015");
+				}
+				dialog.dismiss();
+			}
+		});
+
+		dialog.show();
 	}
 
 	public void next(View view) {
 		Intent intent;
+
 		if (hasPickup == true) {
 			intent = new Intent(this, PickupActivity.class);
 		} else {
 			intent = new Intent(this, TalksActivity.class);
 		}
+
+		EditText accommodationStartDate = (EditText) findViewById(R.id.accommodationStartDate);
+
+		accommodationData.setStartDate("dd-MM-yyyy", accommodationStartDate
+				.getText().toString());
 
 		userData.setAccommodationData(accommodationData);
 
@@ -161,11 +303,34 @@ public class AccomodationActivity extends ActionBarActivity implements
 		} else if (parent.getId() == R.id.bedsCount) {
 			accommodationData.setBedsCount(parent.getItemAtPosition(position)
 					.toString());
+		} else if (parent.getId() == R.id.daysCount) {
+			accommodationData.setDaysCount(parent.getItemAtPosition(position)
+					.toString());
 		}
 	}
 
 	@Override
 	public void onNothingSelected(AdapterView<?> parent) {
 
+	}
+
+	@Override
+	public void onFocusChange(View view, boolean hasFocus) {
+		if (view.getId() == R.id.accommodationStartDate && hasFocus) {
+			InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+			imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+
+			displayCalendar();
+		}
+	}
+
+	@Override
+	public void onClick(View view) {
+		if (view.getId() == R.id.accommodationStartDate) {
+			InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+			imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+
+			displayCalendar();
+		}
 	}
 }
