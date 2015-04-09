2f65f1f05a08cfbfc04275e82e6c024dd67b8fe5
fraggerfox <santhosh.raju@gmail.com>
2015-04-09 22:59:37 +0530

    Added a couple of Interfaces that helps out with writing Automated Functional testing of Async Tasks
    Details of how it works can be found in the comments of the class.

--- android/src/foo/fruitfox/tasks/UserDataWebAPITask.java
+++ android/src/foo/fruitfox/tasks/UserDataWebAPITask.java
@@ -6,7 +6,15 @@ import foo.fruitfox.helpers.NetworkHelper;
 
 public class UserDataWebAPITask extends AsyncTask<String, Integer, String[]> {
 	private AsyncResponseListener callback;
+	private AsyncJobNotifierAccessors unitTestNotifierAccessors;
 
+	/**
+	 * 
+	 * An activity using this Async Task class to execute it's HTTP requests in
+	 * background should implement this interface if they wish to get the
+	 * response of the executed HTTP methods
+	 * 
+	 */
 	public interface AsyncResponseListener {
 		/**
 		 * 
@@ -21,9 +29,41 @@ public class UserDataWebAPITask extends AsyncTask<String, Integer, String[]> {
 		void postAsyncTaskCallback(String responseBody, String responseCode);
 	}
 
+	/**
+	 * 
+	 * A functional "test" wishing to test an activity that does Async Tasks
+	 * must implement this interface so that the notification can get through
+	 * and the CountDownLatch object
+	 * 
+	 */
+	public interface AsyncJobNotifier {
+		void executionDone();
+	}
+
+	/**
+	 * 
+	 * An activity undergoing functional tests which utilizes this Async Task
+	 * class, should implement this interface so that the functional test is
+	 * notified when the Async Job is completed
+	 * 
+	 */
+	public interface AsyncJobNotifierAccessors {
+		public AsyncJobNotifier getUnitTestNotifier();
+
+		public void setUnitTestNotifier(AsyncJobNotifier unitTestNotifier);
+	}
+
+	public UserDataWebAPITask(Context context, AsyncResponseListener callback,
+			AsyncJobNotifierAccessors notifierAccessors) {
+		super();
+		this.callback = callback;
+		this.unitTestNotifierAccessors = notifierAccessors;
+	}
+
 	public UserDataWebAPITask(Context context, AsyncResponseListener callback) {
 		super();
 		this.callback = callback;
+		this.unitTestNotifierAccessors = null;
 	}
 
 	@Override
@@ -51,5 +91,10 @@ public class UserDataWebAPITask extends AsyncTask<String, Integer, String[]> {
 		String responseCode = result[0];
 
 		callback.postAsyncTaskCallback(responseBody, responseCode);
+
+		if (unitTestNotifierAccessors != null
+				&& unitTestNotifierAccessors.getUnitTestNotifier() != null) {
+			unitTestNotifierAccessors.getUnitTestNotifier().executionDone();
+		}
 	}
 }
